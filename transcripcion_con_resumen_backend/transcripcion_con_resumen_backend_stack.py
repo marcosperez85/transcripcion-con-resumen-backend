2b153a13ac84ad82a2b09924568dfcde5e77a397
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-05-02 20:47:36 -0300

    Declaro el stack inicial de funciones Lambda

--- transcripcion_con_resumen_backend/transcripcion_con_resumen_backend_stack.py
+++ transcripcion_con_resumen_backend/transcripcion_con_resumen_backend_stack.py
@@ -1,6 +1,9 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as lambda_,
+    aws_s3 as s3,
+    aws_iam as iam,
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -17,3 +20,48 @@ class TranscripcionConResumenBackendStack(Stack):
         #     self, "TranscripcionConResumenBackendQueue",
         #     visibility_timeout=Duration.seconds(300),
         # )
+
+        # Buckets existentes (o creación de nuevos bucket)
+        bucket_general = s3.Bucket.from_bucket_name(self, "BucketGeneral", "transcripcion-con-resumen")
+
+        # Lambda de transcripción
+        lambda_transcribir = lambda_.Function(self, "LambdaTranscribir",
+            runtime = lambda_.Runtime.PYTHON_3_12,
+            handler = "lambda_function.handler",
+            code = lambda_.Code.from_asset("lambda/transcribir"),
+            environment = {
+                "BUCKET": bucket_general.bucket_name,
+                "PREFIX_DESTINO": "transcripciones/"
+            },
+            timeout = Duration.minutes(5),
+            memory_size = 512
+        )
+
+        lambda_formatear = lambda_.Function(self, "LambdaFormatear",
+            runtime = lambda_.Runtime.PYTHON_3_12,
+            handler = "lambda_function.handler",
+            code = lambda_.Code.from_asset("lambda/formatear"),
+            environment = {
+                "BUCKET": bucket_general.bucket_name,
+                "PREFIX_DESTINO": "transcripciones/"                
+            },
+            timeout = Duration.minutes(5),
+            memory_size = 512
+        )
+
+        lambda_resumir = lambda_.Function(self, "LambdaResumir",
+            runtime = lambda_.Runtime.PYTHON_3_12,
+            handler = "lambda_function.handler",
+            code = lambda_.Code.from_asset("lambda/formatear"),
+            environment = {
+                "BUCKET": bucket_general.bucket_name,
+                "PREFIX_DESTINO" : "resumenes/"
+            },
+            timeout = Duration.minutes(5),
+            memory_size = 512
+        )
+
+        # Permisos para leer y escribir en el bucket
+        bucket_general.grant_read_write(lambda_transcribir)
+        bucket_general.grant_read_write(lambda_formatear)
+        bucket_general.grant_read_write(lambda_resumir)
